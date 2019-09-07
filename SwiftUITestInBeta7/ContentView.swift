a559bd51691480db1acb9830fa7cedfe638a56d8
xunianqiang <xunianqiang@bytedance.com>
2019-09-07 15:35:29 +0800

    Initial Commit

---
+++ SwiftUITestInBeta7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITestInBeta7
+//
+//  Created by xunianqiang on 2019/9/7.
+//  Copyright © 2019 bytedance. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
